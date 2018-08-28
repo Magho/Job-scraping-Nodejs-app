19c7ef0db680d64a546c2dfd43a2bdc2349db3b0
Magho <mmaghraby134@gmail.com>
2018-08-28 16:30:05 +0200

    update get method to get jobs info from indreedAPI & update index.hbs to show the results

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 const handlebars = require('express-handlebars');
@@ -15,7 +15,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
-    res.render("index", {title:"Jobby"})
+    let url = `https://indreed.herokuapp.com/api/jobs?q=web+developer&limit=100`;
+    axios({
+        method: 'get',
+        url
+    })
+    .then(function (response) {
+        let jobs = response.data;
+        res.render("index", { title: "Jobby", jobs: jobs});
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
 });
 
 app.listen(app.get('PORT'), function () {
